f92d2f4873bd22a8200e8286ef1caa66f4a8e06f
Przemek Matylla <hello@hookblaze.com>
2016-01-19 11:53:41 +0100

    Code cleanup, deps update

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-var through = require('through2'),
-    request = require('request'),
-    Kraken = require('kraken'),
-    pretty = require('pretty-bytes'),
-    gutil = require('gulp-util'),
-    chalk = require('chalk'),
-    isGIF = require('is-gif'),
-    isPNG = require('is-png'),
-    isJPG = require('is-jpg'),
-    fs = require('fs');
+var through = require("through2"),
+    request = require("request"),
+    Kraken  = require("kraken"),
+    pretty  = require("pretty-bytes"),
+    gutil   = require("gulp-util"),
+    chalk   = require("chalk"),
+    isGIF   = require("is-gif"),
+    isPNG   = require("is-png"),
+    isJPG   = require("is-jpg"),
+    fs      = require("fs");
 
 module.exports = function (options) {
     options = options || {};
 
     if (!options.key || !options.secret) {
-        throw new gutil.PluginError('gulp-kraken', 'Please provide a valid Kraken API key and secret');
+        throw new gutil.PluginError("gulp-kraken", "Please provide a valid Kraken API key and secret");
     }
 
     var total = {
@@ -28,14 +28,14 @@ module.exports = function (options) {
         }
 
         if (file.isStream()) {
-            this.emit('error', new gutil.PluginError('gulp-kraken', 'Streaming not supported'));
+            this.emit("error", new gutil.PluginError("gulp-kraken", "Streaming not supported"));
             return cb();
         }
 
         var isSupported = !isGIF(file.path) || !isPNG(file.path) || !isJPG(file.path);
 
         if (!isSupported) {
-            gutil.log('gulp-kraken: Skipping unsupported image ' + chalk.blue(file.relative));
+            gutil.log("gulp-kraken: Skipping unsupported image " + chalk.blue(file.relative));
             return cb(null, file);
         }
 
@@ -52,7 +52,7 @@ module.exports = function (options) {
 
         kraken.upload(opts, function (data) {
             if (!data.success) {
-                return cb(new gutil.PluginError('gulp-kraken:', data.message));
+                return cb(new gutil.PluginError("gulp-kraken:", data.message));
             }
 
             var originalSize = data.original_size,
@@ -60,8 +60,8 @@ module.exports = function (options) {
                 savings = data.saved_bytes;
 
             var percent = (((savings) * 100) / originalSize).toFixed(2),
-                savedMsg = 'saved ' + pretty(savings) + ' - ' + percent + '%',
-                msg = savings > 0 ? savedMsg : 'already optimized';
+                savedMsg = "saved " + pretty(savings) + " - " + percent + "%",
+                msg = savings > 0 ? savedMsg : "already optimized";
 
             total.bytes += originalSize;
             total.kraked += krakedSize;
@@ -69,22 +69,22 @@ module.exports = function (options) {
 
             request(data.kraked_url, function (err) {
                 if (err) {
-                    return cb(new gutil.PluginError('gulp-kraken:', err));
+                    return cb(new gutil.PluginError("gulp-kraken:", err));
                 }
 
-                gutil.log('gulp-kraken:', chalk.green('✔ ') + file.relative + chalk.gray(' (' + msg + ')'));
+                gutil.log("gulp-kraken:", chalk.green("✔ ") + file.relative + chalk.gray(" (" + msg + ")"));
                 cb(null, file);
             }).pipe(fs.createWriteStream(file.path));
         });
     }, function (cb) {
-        var percent = (((total.bytes - total.kraked) * 100) / total.bytes).toFixed(2);
-            savings = total.bytes - total.kraked;
-            msg = 'All done. Kraked ' + total.files + ' image';
+        var percent = (((total.bytes - total.kraked) * 100) / total.bytes).toFixed(2),
+            savings = total.bytes - total.kraked,
+            msg = "All done. Kraked " + total.files + " image";
 
-        msg += total.files === 1 ? '' : 's';
-        msg += chalk.gray(' (saved ' + pretty(savings) + ' - ' + percent + '%)');
+        msg += total.files === 1 ? "" : "s";
+        msg += chalk.gray(" (saved " + pretty(savings) + " - " + percent + "%)");
 
-        gutil.log('gulp-kraken:', msg);
+        gutil.log("gulp-kraken:", msg);
         cb();
     });
 };
